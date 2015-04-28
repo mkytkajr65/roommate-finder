73685821d293d3a438698eafb4b31e01e94d21cc
mkytkajr65 <mkytkajr@gmail.com>
2015-04-28 13:34:48 -0400

    matching algorithm

--- functions/matching_algorithm.php
+++ functions/matching_algorithm.php
@@ -13,6 +13,7 @@ function getMatchScore($user_1, $user_2)
 	$matches = 0;
 	$totalQuestions = count($questions);
 
+
 	foreach ($questions as  $question)
 	{
 		if($question->type_id != 4)//checkboxes need to be handled differently
@@ -22,17 +23,35 @@ function getMatchScore($user_1, $user_2)
 			$answers2 = $db->query("SELECT * FROM answers WHERE user_id = ? AND question_id = ?", array($user_2, $question->id));
 			$answers2 = $answers2->first();
 
-			
+			$preference_rating_max = 5;
 
-
-			if($answers1->value === $answers2->value)
+			if($question->type_id == 2)//preference slider
 			{
-				$matches++;
+				if($answers1->value === $answers2->value)//values match
+				{
+					if($answers1->preference_rating)//preference rating exists
+					{
+						$matches += ($answers1->preference_rating/$preference_rating_max);
+					}
+					else//no preference rating but still matches (shouldnt get here)
+					{
+						$matches++;
+					}
+				}
+			}
+			else//
+			{
+				if($answers1->value === $answers2->value)//values match but not preference slider
+				{
+					$matches++;
+				}
 			}
 			
 		}
 	}
 
+	echo $matches . "<br>";
+	echo $totalQuestions . "<br>";
 	$percentage = ($matches / $totalQuestions) * 100;
 	echo $percentage;
 }
