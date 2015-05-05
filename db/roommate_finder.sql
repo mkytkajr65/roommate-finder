4aac329c2dadbfbe1322a71b1cdb188bc1ade965
mkytkajr65 <mkytkajr@gmail.com>
2015-05-05 14:14:53 -0400

    database

--- db/roommate_finder.sql
+++ db/roommate_finder.sql
@@ -1,14 +1,20 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.5
+-- version 4.2.7
 -- http://www.phpmyadmin.net
 --
--- Host: localhost:8889
--- Generation Time: May 04, 2015 at 04:53 PM
--- Server version: 5.5.38
--- PHP Version: 5.5.14
+-- Host: localhost:3306
+-- Generation Time: May 05, 2015 at 06:14 PM
+-- Server version: 5.5.41-log
+-- PHP Version: 5.6.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `roommate_finder`
@@ -20,191 +26,51 @@ SET time_zone = "+00:00";
 -- Table structure for table `answers`
 --
 
-CREATE TABLE `answers` (
+CREATE TABLE IF NOT EXISTS `answers` (
 `id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `question_id` int(11) NOT NULL,
   `value` int(11) NOT NULL,
   `preference_rating` int(11) DEFAULT NULL COMMENT 'numbers 1-5'
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=622 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=33 ;
 
 --
 -- Dumping data for table `answers`
 --
 
 INSERT INTO `answers` (`id`, `user_id`, `question_id`, `value`, `preference_rating`) VALUES
-(4, 1, 1, 0, 1),
-(6, 1, 3, 0, NULL),
-(7, 1, 4, 1, 1),
-(8, 1, 5, 2, NULL),
-(9, 1, 6, 0, 3),
-(10, 1, 7, 3, NULL),
-(11, 1, 8, 0, 4),
-(12, 1, 9, 1, NULL),
-(13, 1, 10, 1, 5),
-(14, 1, 11, 1, NULL),
-(15, 1, 12, 1, NULL),
-(16, 1, 13, 3, NULL),
-(17, 1, 14, 1, NULL),
-(18, 1, 15, 1, NULL),
-(19, 1, 16, 0, NULL),
-(20, 1, 17, 1, NULL),
-(26, 2, 1, 1, 2),
-(28, 2, 3, 0, NULL),
-(29, 2, 4, 0, 5),
-(30, 2, 5, 1, NULL),
-(31, 2, 6, 0, 5),
-(32, 2, 7, 3, NULL),
-(33, 2, 8, 0, 1),
-(34, 2, 9, 1, NULL),
-(35, 2, 10, 1, 4),
-(36, 2, 11, 1, NULL),
-(37, 2, 12, 1, NULL),
-(38, 2, 13, 2, NULL),
-(39, 2, 14, 1, NULL),
-(40, 2, 15, 1, NULL),
-(41, 2, 16, 0, NULL),
-(42, 2, 17, 0, NULL),
-(56, 1, 18, 1, NULL),
-(57, 1, 18, 4, NULL),
-(58, 4, 11, 1, NULL),
-(59, 4, 12, 2, NULL),
-(60, 4, 13, 2, NULL),
-(61, 4, 14, 0, NULL),
-(62, 4, 15, 1, NULL),
-(63, 4, 16, 0, NULL),
-(64, 4, 5, 2, NULL),
-(65, 4, 6, 0, 5),
-(66, 4, 7, 4, NULL),
-(67, 4, 17, 0, NULL),
-(70, 4, 8, 1, 1),
-(71, 4, 9, 2, NULL),
-(72, 4, 10, 0, 1),
-(73, 4, 1, 0, 1),
-(75, 4, 3, 0, NULL),
-(76, 4, 4, 0, 5),
-(77, 3, 5, 2, NULL),
-(78, 3, 6, 0, 3),
-(79, 3, 7, 1, NULL),
-(80, 3, 1, 0, 1),
-(82, 3, 3, 0, NULL),
-(83, 3, 4, 1, 1),
-(84, 3, 11, 0, NULL),
-(85, 3, 12, 1, NULL),
-(86, 3, 13, 2, NULL),
-(87, 3, 8, 1, 1),
-(88, 3, 9, 0, NULL),
-(89, 3, 10, 0, 2),
-(90, 3, 17, 0, NULL),
-(93, 3, 14, 1, NULL),
-(94, 3, 15, 1, NULL),
-(95, 3, 16, 1, NULL),
-(113, 3, 18, 0, NULL),
-(114, 3, 18, 5, NULL),
-(115, 3, 18, 6, NULL),
-(116, 3, 18, 7, NULL),
-(132, 5, 5, 1, NULL),
-(133, 5, 6, 0, 4),
-(134, 5, 7, 3, NULL),
-(135, 5, 8, 0, 5),
-(136, 5, 9, 1, NULL),
-(137, 5, 10, 0, 1),
-(138, 5, 1, 0, 1),
-(140, 5, 3, 1, NULL),
-(141, 5, 4, 1, 1),
-(142, 5, 11, 0, NULL),
-(143, 5, 12, 2, NULL),
-(144, 5, 13, 2, NULL),
-(145, 5, 14, 2, NULL),
-(146, 5, 15, 1, NULL),
-(147, 5, 16, 1, NULL),
-(148, 5, 17, 0, NULL),
-(153, 6, 8, 0, 5),
-(154, 6, 9, 2, NULL),
-(155, 6, 10, 0, 1),
-(156, 6, 5, 1, NULL),
-(157, 6, 6, 0, 2),
-(158, 6, 7, 3, NULL),
-(159, 6, 1, 0, 5),
-(161, 6, 3, 1, NULL),
-(162, 6, 4, 1, 3),
-(163, 6, 11, 1, NULL),
-(164, 6, 12, 1, NULL),
-(165, 6, 13, 2, NULL),
-(166, 6, 17, 0, NULL),
-(169, 6, 14, 2, NULL),
-(170, 6, 15, 2, NULL),
-(171, 6, 16, 0, NULL),
-(172, 6, 18, 5, NULL),
-(173, 7, 1, 0, 1),
-(175, 7, 3, 0, NULL),
-(176, 7, 4, 1, 1),
-(177, 7, 8, 1, 4),
-(178, 7, 9, 1, NULL),
-(179, 7, 10, 0, 1),
-(180, 7, 11, 0, NULL),
-(181, 7, 12, 1, NULL),
-(182, 7, 13, 3, NULL),
-(183, 7, 5, 1, NULL),
-(184, 7, 6, 0, 1),
-(185, 7, 7, 4, NULL),
-(186, 7, 14, 0, NULL),
-(187, 7, 15, 1, NULL),
-(188, 7, 16, 0, NULL),
-(189, 7, 17, 0, NULL),
-(190, 7, 18, 0, NULL),
-(191, 7, 18, 1, NULL),
-(192, 7, 18, 3, NULL),
-(193, 7, 18, 6, NULL),
-(196, 8, 1, 0, 1),
-(198, 8, 3, 0, NULL),
-(199, 8, 4, 1, 1),
-(200, 8, 5, 2, NULL),
-(201, 8, 6, 0, 1),
-(202, 8, 7, 2, NULL),
-(203, 8, 8, 0, 1),
-(204, 8, 9, 0, NULL),
-(205, 8, 10, 0, 1),
-(206, 8, 11, 1, NULL),
-(207, 8, 12, 1, NULL),
-(208, 8, 13, 2, NULL),
-(209, 8, 14, 2, NULL),
-(210, 8, 15, 1, NULL),
-(211, 8, 16, 0, NULL),
-(212, 8, 17, 0, NULL),
-(216, 8, 18, 5, NULL),
-(532, 5, 18, 0, NULL),
-(533, 5, 18, 6, NULL),
-(534, 9, 1, 0, 1),
-(535, 9, 3, 0, NULL),
-(536, 9, 4, 0, 3),
-(537, 9, 5, 1, NULL),
-(538, 9, 6, 0, 5),
-(539, 9, 7, 3, NULL),
-(540, 9, 8, 0, 2),
-(541, 9, 9, 1, NULL),
-(542, 9, 10, 1, 5),
-(543, 9, 11, 1, NULL),
-(544, 9, 12, 0, NULL),
-(545, 9, 13, 2, NULL),
-(546, 9, 14, 1, NULL),
-(547, 9, 15, 1, NULL),
-(548, 9, 16, 0, NULL),
-(549, 9, 17, 0, NULL),
-(552, 9, 18, 0, NULL),
-(553, 9, 18, 1, NULL),
-(554, 9, 18, 2, NULL),
-(555, 9, 18, 4, NULL),
-(556, 9, 18, 5, NULL),
-(557, 9, 18, 6, NULL),
-(558, 9, 18, 7, NULL),
-(615, 2, 18, 0, NULL),
-(616, 2, 18, 1, NULL),
-(617, 2, 18, 2, NULL),
-(618, 2, 18, 4, NULL),
-(619, 2, 18, 5, NULL),
-(620, 2, 18, 6, NULL),
-(621, 2, 18, 7, NULL);
+(1, 444023, 1, 0, 1),
+(2, 444023, 3, 0, NULL),
+(3, 444023, 4, 0, 1),
+(4, 444023, 5, 0, NULL),
+(5, 444023, 6, 0, 1),
+(6, 444023, 7, 1, NULL),
+(7, 444023, 11, 0, NULL),
+(8, 444023, 12, 0, NULL),
+(9, 444023, 13, 0, NULL),
+(10, 444023, 8, 0, 1),
+(11, 444023, 9, 0, NULL),
+(12, 444023, 10, 0, 1),
+(13, 444023, 14, 0, NULL),
+(14, 444023, 15, 0, NULL),
+(15, 444023, 16, 0, NULL),
+(16, 444023, 17, 0, NULL),
+(17, 452071, 1, 0, 1),
+(18, 452071, 3, 0, NULL),
+(19, 452071, 4, 0, 1),
+(20, 452071, 5, 0, NULL),
+(21, 452071, 6, 0, 1),
+(22, 452071, 7, 0, NULL),
+(23, 452071, 8, 0, 1),
+(24, 452071, 9, 0, NULL),
+(25, 452071, 10, 0, 1),
+(26, 452071, 11, 0, NULL),
+(27, 452071, 12, 0, NULL),
+(28, 452071, 13, 0, NULL),
+(29, 452071, 14, 0, NULL),
+(30, 452071, 15, 0, NULL),
+(31, 452071, 16, 0, NULL),
+(32, 452071, 17, 0, NULL);
 
 -- --------------------------------------------------------
 
@@ -212,12 +78,12 @@ INSERT INTO `answers` (`id`, `user_id`, `question_id`, `value`, `preference_rati
 -- Table structure for table `options`
 --
 
-CREATE TABLE `options` (
+CREATE TABLE IF NOT EXISTS `options` (
 `id` int(11) NOT NULL,
   `name` varchar(50) NOT NULL,
   `question_id` int(11) NOT NULL,
   `value_index` int(11) NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=82 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=83 ;
 
 --
 -- Dumping data for table `options`
@@ -263,7 +129,8 @@ INSERT INTO `options` (`id`, `name`, `question_id`, `value_index`) VALUES
 (78, 'Hard/Metal', 18, 5),
 (79, 'Rap/Hip Hop', 18, 6),
 (80, 'Rock', 18, 7),
-(81, 'Soft Rock/Pop', 18, 8);
+(81, 'Soft Rock/Pop', 18, 8),
+(82, 'EDM', 18, 9);
 
 -- --------------------------------------------------------
 
@@ -271,26 +138,19 @@ INSERT INTO `options` (`id`, `name`, `question_id`, `value_index`) VALUES
 -- Table structure for table `public_answer`
 --
 
-CREATE TABLE `public_answer` (
+CREATE TABLE IF NOT EXISTS `public_answer` (
 `id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `value` tinyint(4) NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=11 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 --
 -- Dumping data for table `public_answer`
 --
 
 INSERT INTO `public_answer` (`id`, `user_id`, `value`) VALUES
-(1, 1, 1),
-(3, 2, 1),
-(4, 3, 1),
-(5, 4, 1),
-(6, 5, 0),
-(7, 6, 1),
-(8, 7, 1),
-(9, 8, 1),
-(10, 9, 1);
+(1, 444023, 1),
+(2, 452071, 1);
 
 -- --------------------------------------------------------
 
@@ -298,7 +158,7 @@ INSERT INTO `public_answer` (`id`, `user_id`, `value`) VALUES
 -- Table structure for table `public_question`
 --
 
-CREATE TABLE `public_question` (
+CREATE TABLE IF NOT EXISTS `public_question` (
 `id` int(11) NOT NULL,
   `question` varchar(100) NOT NULL
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
@@ -316,7 +176,7 @@ INSERT INTO `public_question` (`id`, `question`) VALUES
 -- Table structure for table `questions`
 --
 
-CREATE TABLE `questions` (
+CREATE TABLE IF NOT EXISTS `questions` (
 `id` int(11) NOT NULL,
   `question` varchar(500) NOT NULL,
   `tab_id` int(11) NOT NULL,
@@ -344,7 +204,7 @@ INSERT INTO `questions` (`id`, `question`, `tab_id`, `type_id`) VALUES
 (15, 'How comfortable are you sharing your belongings?', 5, 3),
 (16, 'How do you react when something is bothering you?', 5, 3),
 (17, 'I prefer a roommate with:', 6, 3),
-(18, 'My music tastes: ', 6, 4);
+(18, 'My music tastes:', 6, 4);
 
 -- --------------------------------------------------------
 
@@ -352,7 +212,7 @@ INSERT INTO `questions` (`id`, `question`, `tab_id`, `type_id`) VALUES
 -- Table structure for table `question_types`
 --
 
-CREATE TABLE `question_types` (
+CREATE TABLE IF NOT EXISTS `question_types` (
 `id` int(11) NOT NULL,
   `name` varchar(50) NOT NULL
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
@@ -373,7 +233,7 @@ INSERT INTO `question_types` (`id`, `name`) VALUES
 -- Table structure for table `tabs`
 --
 
-CREATE TABLE `tabs` (
+CREATE TABLE IF NOT EXISTS `tabs` (
 `id` int(11) NOT NULL,
   `name` varchar(30) NOT NULL
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
@@ -396,34 +256,26 @@ INSERT INTO `tabs` (`id`, `name`) VALUES
 -- Table structure for table `user`
 --
 
-CREATE TABLE `user` (
-`id` int(11) NOT NULL,
-  `picture` varchar(50) NOT NULL,
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL,
   `password` varchar(64) NOT NULL,
   `first_name` varchar(20) NOT NULL,
   `last_name` varchar(30) NOT NULL,
-  `account_type` varchar(20) NOT NULL,
+  `account_type` varchar(20) NOT NULL COMMENT 'if administrative account, account_type = admin',
   `email` varchar(50) NOT NULL,
-  `facebook` varchar(50) NOT NULL DEFAULT 'https://www.facebook.com/yourusername',
+  `facebook` varchar(50) NOT NULL,
   `public` tinyint(4) NOT NULL DEFAULT '1',
   `gender` int(11) NOT NULL COMMENT '0 = female 1 = male',
   `status` smallint(6) NOT NULL COMMENT '0 = new student, 1 = current student'
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id`, `picture`, `password`, `first_name`, `last_name`, `account_type`, `email`, `facebook`, `public`, `gender`, `status`) VALUES
-(1, 'dummy.jpg', 'grover', 'Michael', 'Curtis', 'admin', 'michaelcurtis@gmail.com', '', 1, 1, 0),
-(2, 'dummy.jpg', 'grover', 'Michael', 'Kytka', 'admin', 'michaelkytka@gmail.com', 'https://www.facebook.com/Michael.Kytka.Jr', 1, 1, 1),
-(3, 'dummy.jpg', 'grover', 'Zach', 'Nafziger', 'admin', 'yourmom@gmail.com', 'https://www.facebook.com/znafziger', 1, 1, 0),
-(4, 'dummy.jpg', 'grover', 'Josh', 'Walton', 'admin', 'joshwalton@gmail.com', '', 1, 1, 1),
-(5, 'dummy.jpg', 'grover', 'Tyler', 'Depew', '', 'tylerdepew@gmail.com', 'https://www.facebook.com', 1, 1, 0),
-(6, 'dummy.jpg', 'grover', 'Sean', 'Oriordan', '', 'seanoriordan@gmail.com', 'https://www.facebook.com', 1, 1, 0),
-(7, 'dummy.jpg', 'grover', 'Ryan', 'Duran', '', 'ryanduran@gmail.com', 'https://www.facebook.com', 1, 1, 0),
-(8, 'dummy.jpg', 'grover', 'James', 'Payne', '', 'jamespayne@gmail.com', 'https://www.facebook.com', 1, 1, 0),
-(9, 'dummy.jpg', 'grover', 'Michael', 'Kitkat', '', 'kytka@gmail.com', 'https://www.facebook.com', 1, 0, 0);
+INSERT INTO `user` (`id`, `password`, `first_name`, `last_name`, `account_type`, `email`, `facebook`, `public`, `gender`, `status`) VALUES
+(444023, 'grover', 'Michael', 'Kytka', 'admin', 'kytkama1@gcc.edu', '', 1, 1, 0),
+(452071, 'grover', 'Zach', 'Nafziger', '', 'zn@gcc.edu', 'https://www.facebook.com/PittNomad?fref=ts', 1, 1, 0);
 
 --
 -- Indexes for dumped tables
@@ -485,17 +337,17 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `answers`
 --
 ALTER TABLE `answers`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=622;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=33;
 --
 -- AUTO_INCREMENT for table `options`
 --
 ALTER TABLE `options`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=82;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=83;
 --
 -- AUTO_INCREMENT for table `public_answer`
 --
 ALTER TABLE `public_answer`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=11;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `public_question`
 --
@@ -516,11 +368,6 @@ MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
 --
 ALTER TABLE `tabs`
 MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
---
--- AUTO_INCREMENT for table `user`
---
-ALTER TABLE `user`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=10;
 --
 -- Constraints for dumped tables
 --
@@ -550,3 +397,7 @@ ADD CONSTRAINT `FK_public_answer_user_user_id` FOREIGN KEY (`user_id`) REFERENCE
 ALTER TABLE `questions`
 ADD CONSTRAINT `FK_questions_question_types_type_id` FOREIGN KEY (`type_id`) REFERENCES `question_types` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
 ADD CONSTRAINT `FK_questions_tabs_tab_id` FOREIGN KEY (`tab_id`) REFERENCES `tabs` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
